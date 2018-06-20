c39e6a3c8a939c2915d0725d8e609b497ca7b133
Matheus Mesquita <matheusmesquita.168@gmail.com>
2018-06-20 12:50:55 -0300

    suporte ao a2hs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+window.addEventListener("beforeinstallprompt", function(event) {
+  console.log("A2HS", event);
+
+  window.a2hsEvent = event;
+});
+
 registerServiceWorker();
