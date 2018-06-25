e350122e28f28d0101d4192a5c840cf36e4e5bf0
Mariana Medeiros Ruddy Santos <mariana.ruddy@b2wdigital.com>
2018-06-25 14:17:40 -0300

    alteracao2

--- workbox-config.js
+++ workbox-config.js
@@ -9,6 +9,25 @@ module.exports = {
         "^https://use.fontawesome.com/releases/v5.0.7/js/all.js"
       ),
       handler: "staleWhileRevalidate"
+    },
+    {
+      urlPattern: new RegExp("/\.(?:png|gif|jpg|svg)$/"),
+      handler: "cacheFirst",
+      options: {
+        expiration: {
+          maxEntries: 5,
+          maxAgeSeconds: 60*60*24*30
+        }
+      }
+    },
+    {
+      urlPattern: new RegExp("^https://swapi.co/api/"),
+      handler: "staleWhileRevalidate",
+      options: {
+        expiration: {
+          maxEntries: 3
+        }
+      }
     }
   ]
 };
